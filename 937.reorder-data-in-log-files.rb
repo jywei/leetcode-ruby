c4858712dd5d57780d5101eb7a206354da8593a0
Roy Wei <roywjy@gmail.com>
2022-05-31 17:14:00 +1000

    Add reorder_log_files

--- 937.reorder-data-in-log-files.rb
+++ 937.reorder-data-in-log-files.rb
@@ -1,5 +1,17 @@
 # @param {String[]} logs
 # @return {String[]}
 def reorder_log_files(logs)
+  num_logs = []
+  str_logs = []
+  logs.each do |log|
+    if /\d/.match?(log[-1])
+      num_logs << log
+    else
+      str_logs << log
+    end
+  end
+
+  str_logs.sort_by! { |log| [ log.split[1..-1].join(' '), log.split[0] ] }
+  str_logs + num_logs
 
 end
