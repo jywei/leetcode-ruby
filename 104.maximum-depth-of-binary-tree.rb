82cfcb86236906ba7e87040ef135929b668128c4
Roy Wei <roywjy@gmail.com>
2022-06-01 12:07:12 +1000

    Adjust stylings

--- 104.maximum-depth-of-binary-tree.rb
+++ 104.maximum-depth-of-binary-tree.rb
@@ -10,6 +10,6 @@
 # @param {TreeNode} root
 # @return {Integer}
 def max_depth(root)
-    return 0 if root.nil?
-    [max_depth(root.left), max_depth(root.right)].max + 1
+  return 0 if root.nil?
+  [max_depth(root.left), max_depth(root.right)].max + 1
 end
